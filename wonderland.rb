d8ebcb2e2cda8a39c04965edc1569f0e8ae36de9
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-26 20:04:53 +0100

    setting the attribute of a hash with key and value indicating the projects title

--- wonderland.rb
+++ wonderland.rb
@@ -1,3 +1,8 @@
 require 'ruby2d'
 
+# setting the title attribute so that it will display it once it's open.
+# passing it a hash with a symbol which is known as my key stating title.
+# Then my value as a string which is the name of the project.
+set( {:title => "Acrylic Wonderland" } )
+
 show
