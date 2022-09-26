afe4b826b3e6a7083ff39aa9dd7633b59e180c85
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-27 00:56:57 +0100

    setting the title attribute with value of project name, set the background colour to purple and styled the width and height dimensions for the game window itself

--- wonderland.rb
+++ wonderland.rb
@@ -1,8 +1,16 @@
-require 'ruby2d'
+require "ruby2d"
+# require 'bundler/setup'
+# require 'rubygems'
+# Ruby2D::Window
 
+# title for the window.
 # setting the title attribute so that it will display it once it's open.
 # passing it a hash with a symbol which is known as my key stating title.
 # Then my value as a string which is the name of the project.
 set( {:title => "Acrylic Wonderland" } )
+
+set(background: "purple", width: 640, height: 480)
+
+puts 'hello world'
 
 show
